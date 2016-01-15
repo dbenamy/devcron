e923a2cba9dd988b361757b50d26e8c0d995b09f
Daniel Benamy <daniel@benamy.info>
2016-01-14 23:45:48 -0500

    Updated setup.py for modern packaging.

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
+# To use a consistent encoding
+from codecs import open
 import os
+from setuptools import setup
+
 readme_file = os.path.join(os.path.dirname(__file__),
-                           'README.txt')
-long_description = open(readme_file).read()
+                           'README.md')
+long_description = open(readme_file, encoding='utf-8').read()
 
 classifiers = [
-    'Development Status :: 3 - Alpha',
+    'Development Status :: 4 - Beta',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: GNU General Public License (GPL)',
-    'Operating System :: OS Independent']
+    'Operating System :: OS Independent',
+
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+]
 
 setup(author='Daniel Benamy',
-      author_email='dbenamy@igicom.com',
+      author_email='daniel@benamy.info',
       classifiers=classifiers,
       description='Cron for working on projects that use crontabs.',
       long_description=long_description,
@@ -24,4 +28,8 @@ setup(author='Daniel Benamy',
       scripts=['devcron.py'],
       url='https://bitbucket.org/dbenamy/devcron/',
       version='0.2',
-     )
+      entry_points={
+          'console_scripts': [
+              'devcron=devcron:main',
+          ],
+      })
