86d5f21759a52a6fc1b2eae80a747bd0311a6b3d
Ilya Skriblovsky <IlyaSkriblovsky@gmail.com>
2016-03-14 18:54:30 +0300

    Added version classifiers for Python 3

--- setup.py
+++ setup.py
@@ -16,6 +16,10 @@ classifiers = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
 ]
 
 setup(author='Daniel Benamy',
