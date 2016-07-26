17762f321e06f7ad773c783cca866ed181f86ec3
Daniel Benamy <daniel@benamy.info>
2016-07-26 01:22:27 -0400

    Ran yapf on setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from codecs import open
 import os
 from setuptools import setup
 
-readme_file = os.path.join(os.path.dirname(__file__),
-                           'README.md')
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(readme_file, encoding='utf-8').read()
 
 classifiers = [
@@ -12,7 +11,6 @@ classifiers = [
     'Intended Audience :: Developers',
     'License :: OSI Approved :: GNU General Public License (GPL)',
     'Operating System :: OS Independent',
-
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3',
@@ -22,21 +20,22 @@ classifiers = [
     'Programming Language :: Python :: 3.5',
 ]
 
-setup(author='Daniel Benamy',
-      author_email='daniel@benamy.info',
-      classifiers=classifiers,
-      description='Cron for working on projects that use crontabs.',
-      long_description=long_description,
-      name='devcron',
-      py_modules=['devcron'],
-      scripts=['devcron.py'],
-      install_requires=['six'],
-      tests_require=['mock'],
-      test_suite='tests',
-      url='https://bitbucket.org/dbenamy/devcron/',
-      version='0.4',
-      entry_points={
-          'console_scripts': [
-              'devcron=devcron:main',
-          ],
-      })
+setup(
+    author='Daniel Benamy',
+    author_email='daniel@benamy.info',
+    classifiers=classifiers,
+    description='Cron for working on projects that use crontabs.',
+    long_description=long_description,
+    name='devcron',
+    py_modules=['devcron'],
+    scripts=['devcron.py'],
+    install_requires=['six'],
+    tests_require=['mock'],
+    test_suite='tests',
+    url='https://bitbucket.org/dbenamy/devcron/',
+    version='0.4',
+    entry_points={
+        'console_scripts': [
+            'devcron=devcron:main',
+        ],
+    })
